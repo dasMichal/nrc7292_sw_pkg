2ff2f4f75328a8f1f4e3a5dbe5cf6c12eaa579e3
Michał <57588949+dasMichal@users.noreply.github.com>
2025-11-30 22:48:04 +0100

    Make update.sh user-independent with dynamic paths

--- package/evk/sw_pkg/update.sh
+++ package/evk/sw_pkg/update.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-if [ -d "/home/pi/nrc_pkg_bk" ]; then
+#get user name 
+USER_NAME=$(whoami)
+
+if [ -d "/home/$USER_NAME/nrc_pkg_bk" ]; then
  echo "Remove backup folder"
- rm -rf /home/pi/nrc_pkg_bk
+ rm -rf /home/$USER_NAME/nrc_pkg_bk
 fi
-if [ -d "/home/pi/nrc_pkg" ]; then
+if [ -d "/home/$USER_NAME/nrc_pkg" ]; then
  echo "Backup previous package"
- mv /home/pi/nrc_pkg /home/pi/nrc_pkg_bk
+ mv /home/$USER_NAME/nrc_pkg /home/pi/nrc_pkg_bk
 fi
 sleep 1
 
 echo "Copy new package"
 echo "apply nrc_pkg "
-cp -r ./nrc_pkg/  /home/pi/nrc_pkg/
+cp -r ./nrc_pkg/  /home/$USER_NAME/nrc_pkg/
 
 echo "Change mode"
-cd /home/pi/nrc_pkg
+cd /home/$USER_NAME/nrc_pkg
 sudo chmod -R 755 *
-sudo chmod -R 777 /home/pi/nrc_pkg/script/*
-sudo chmod -R 777 /home/pi/nrc_pkg/sw/firmware/copy
+sudo chmod -R 777 /home/$USER_NAME/nrc_pkg/script/*
+sudo chmod -R 777 /home/$USER_NAME/nrc_pkg/sw/firmware/copy
 sleep 1
 
 echo "Done"
